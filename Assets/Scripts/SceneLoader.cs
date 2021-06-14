507d8bb9cba191f1f0596ec7191a577fd5ebb520
DESKTOP-QVDMVKM\ryusa <ryusakamaki311@gmail.com>
2021-06-14 10:19:43 +0900

    タイトルシーンの編集をした

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
+    AudioSource _source;
+    [SerializeField] AudioClip _clip;
+   
+    private void Start()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+    /// <summary>
+    /// 音を鳴らせてSceneを変える
+    /// </summary>
     public void SceneLoad()
     {
-        SceneManager.LoadScene(1);
+        _source.PlayOneShot(_clip);
+        SceneManager.LoadScene("GameScene");
     }
 }
